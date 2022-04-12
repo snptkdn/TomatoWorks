662007e22e1d65cddeca0a52afe9623260aa09ea
snptkdn <400m.wflick@gmail.com>
2022-04-12 11:29:23 +0900

    TODOリストの追加機能を実装した。

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-console.log("App.js: loaded");
+import { element, render } from "./view/html-util.js";
+
 export class App {
-    constructor() {
-        console.log("App initialized");
+    mount() {
+        const formElement = document.querySelector("#js-form");
+        const inputElement = document.querySelector("#js-form-input");
+        const conteinerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        const todoListElement = element`<ul />`;
+        let todoItemCount = 0;
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            todoListElement.appendChild(todoItemElement);
+            render(todoListElement, conteinerElement);
+
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            inputElement.value = ""
+        })
     }
 }
